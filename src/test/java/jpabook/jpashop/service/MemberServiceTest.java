1f3b0fb25230261c839b4e0d83f811555408071b
ss0712.kim <ss0712.kim@samsung.com>
2020-12-28 20:29:53 +0900

    add member service, repository

---
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -0,0 +1,52 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest // spring boot 를 띄운 상태에서 테스트, 없으면 autowired 등 실패함
+@Transactional
+public class MemberServiceTest {
+
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    public void join() throws Exception {
+        //given
+        Member member = new Member();
+        member.setName("kim");
+
+        //when
+        Long saveId = memberService.join(member);
+
+        //then
+        assertEquals(member, memberRepository.findOne(saveId));
+    }
+
+    @Test
+    public void validateDuplicate() throws Exception {
+        //given
+        Member member1 = new Member();
+        member1.setName("kim");
+
+        Member member2 = new Member();
+        member2.setName("kim");
+
+        //when
+        memberService.join(member1);
+        Exception exception = Assertions.assertThrows(IllegalStateException.class, () -> {
+            memberService.join(member2);
+        }); // 예외가 발생해야 함
+
+        //then
+        assertEquals("이미 존재하는 회원입니다.", exception.getMessage());
+
+    }
+}
